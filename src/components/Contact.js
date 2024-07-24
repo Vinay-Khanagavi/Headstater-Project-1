74a74e65f6890d32566dd2b11b2553814b56bfea
vinaykhanagavi <vinaykhanagavi502@gmail.com>
2024-07-24 13:04:25 +0530

    Changes to be committed: 	modified:   public/CNAME 	new file:   src/assets/img/projects/CyberFit.png 	new file:   src/assets/img/projects/FitnessT.png 	deleted:    src/assets/img/projects/KnowledgeGraph.png 	deleted:    src/assets/img/projects/LyricFinder.png 	deleted:    src/assets/img/projects/NLP.png 	deleted:    src/assets/img/projects/Othello.png 	new file:   src/assets/img/projects/TripTally.png 	modified:   src/assets/json/projects.json 	modified:   src/components/Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -31,6 +31,7 @@ const Contact = () => {
         setButtonText('Sending...')
 
         emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+        
             .then((result) => {
                 console.log(result.text)
                 setStatus({
